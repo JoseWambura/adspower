1b96dbaf82553b2dbe6417ef76d255925ad74e61
JoseWambura <133878938+JoseWambura@users.noreply.github.com>
2025-09-30 12:37:37 +0300

    Update jr-main.js

--- jr-main.js
+++ jr-main.js
@@ -81,10 +81,10 @@
 
   const START_DELAY_MS    = Math.floor(Math.random() * (20000 - 15000 + 1)) + 15000; // 15–20s
   const SCROLL_DIST_MIN_PX = 800, SCROLL_DIST_MAX_PX = 1200;
-  const SCROLL_DUR_MIN_MS  = 3000, SCROLL_DUR_MAX_MS  = 5000;
-  const MIN_SCROLL_CYCLES  = Math.floor(Math.random() * (4 - 3 + 1)) + 3; // 3–4 cycles
-  const READ_PAUSE_MIN_MS  = 4000,  READ_PAUSE_MAX_MS  = 5000;
-  const BOTTOM_CONFIRM_MS  = 900;
+  const SCROLL_DUR_MIN_MS  = 5000, SCROLL_DUR_MAX_MS  = 7000;
+  const MIN_SCROLL_CYCLES  = Math.floor(Math.random() * (5 - 3 + 1)) + 3; // 3–4 cycles
+  const READ_PAUSE_MIN_MS  = 6000,  READ_PAUSE_MAX_MS  = 9000;
+  const BOTTOM_CONFIRM_MS  = 9000;
 
   const firedPercents = new Set();
   const BREAKPOINTS = [25, 50, 75, 90, 100];
