ff371bee9ed838e964b947d4642e46026060b208
JoseWambura <133878938+JoseWambura@users.noreply.github.com>
2025-09-30 10:47:58 +0300

    Update jr-main.js

--- jr-main.js
+++ jr-main.js
@@ -287,7 +287,9 @@
     mo.observe(document.documentElement, { childList: true, subtree: true });
   }
 
-  
+  stripExistingMedia();
+  observeNewMedia();
+  setupIframeHider();
   console.log('[ImgBlock] Active. Allowed image hosts:', ALLOW_HOSTS.map(r => r.source).join(', '));
 
   /******************************************************************
