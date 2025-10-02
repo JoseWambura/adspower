792724a90fc585a09207477ba7fb7277d3cb9bb3
JoseWambura <133878938+JoseWambura@users.noreply.github.com>
2025-10-02 21:22:39 +0300

    Update jr-main.js

--- jr-main.js
+++ jr-main.js
@@ -82,8 +82,8 @@
   const START_DELAY_MS    = Math.floor(Math.random() * (25000 - 20000 + 1)) + 15000; // 15–20s
   const SCROLL_DIST_MIN_PX = 800, SCROLL_DIST_MAX_PX = 1200;
   const SCROLL_DUR_MIN_MS  = 5000, SCROLL_DUR_MAX_MS  = 7000;
-  const MIN_SCROLL_CYCLES = Math.floor(Math.random() * (6 - 5 + 1)) + 5; // 6–7 cycles
-  const READ_PAUSE_MIN_MS  = 7500,  READ_PAUSE_MAX_MS  = 9000;
+  const MIN_SCROLL_CYCLES = Math.floor(Math.random() * (5 - 4 + 1)) + 4; // 6–7 cycles
+  const READ_PAUSE_MIN_MS  = 5500,  READ_PAUSE_MAX_MS  = 7000;
   const BOTTOM_CONFIRM_MS  = 9000;
 
   const firedPercents = new Set();
